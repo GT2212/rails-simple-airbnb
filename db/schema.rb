08e122b380c974af1f465e36c325882efda13391
gabi <gabrielteodorescu2212@yahoo.com>
2024-03-06 15:59:39 +0100

    Final touch without styling

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,15 +12,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_06_135728) do
-  create_table "flats", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.text "description"
-    t.integer "price_per_night"
-    t.integer "number_of_guests"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.1].define(version: 20_240_306_135_728) do
+  create_table 'flats', force: :cascade do |t|
+    t.string 'name'
+    t.string 'address'
+    t.text 'description'
+    t.integer 'price_per_night'
+    t.integer 'number_of_guests'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
